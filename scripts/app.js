89aefc486c9bb2aaae04456d742d3a69a8f324b0
Saquib Uddin Ahmed <saquibuudin@msn.com>
2020-06-25 10:07:40 -0400

    complete project uploaded 20200625

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,7 @@
 //contains all DOM manipulation
 
+$("#searchBtn").on("click", function(){
+    console.log("show Modal")
+    $('#myModal').modal('show')
+
+})
--- scripts/schedule.js
+++ scripts/schedule.js
@@ -2,113 +2,59 @@
 
 const key = '6ecb70d40bmsh6e5782f07713658p17f146jsn581f137859e6';
 
-const getSport = async (sport) => {
+ async function getSport(sport){ 
 
-    const base = 'https://dev132-cricket-live-scores-v1.p.rapidapi.com/matches.php?completedlimit=5&inprogresslimit=5&upcomingLimit=5"'
+    const base = 'https://dev132-cricket-live-scores-v1.p.rapidapi.com/matches.php?completedlimit=5&inprogresslimit=5&upcomingLimit=5'
 
-    const query = `&apiKey=${key}&q=${sport}`;
+    //const query = `&apiKey=${key}&q=${sport}`;
     
-    const settings = {"headers": {
-		"x-rapidapi-host": "dev132-cricket-live-scores-v1.p.rapidapi.com",
-		"x-rapidapi-key": "6ecb70d40bmsh6e5782f07713658p17f146jsn581f137859e6"
-    }}
-
-    const data= await fetch("https://dev132-cricket-live-scores-v1.p.rapidapi.com/matches.php?completedlimit=5&inprogresslimit=5&upcomingLimit=5", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "dev132-cricket-live-scores-v1.p.rapidapi.com",
-		"x-rapidapi-key": "6ecb70d40bmsh6e5782f07713658p17f146jsn581f137859e6"
-	}
-})
-.then(response => {
-    return response.json();
-
- })
-
-//     const response = await fetch(base + query, settings);
-    
-//         const data = await response.json();
-
-    console.log(data.matchList.matches);
-let matches = data.matchList.matches;
-
-    for (let i=0; i < matches.length; i++ ) {
-        //let startTime = matches[i].startDateTime;
-       // console.log('start time: ', startTime);
-
-        //let fromZoneName = data.matchList.matches.series.getElementByTagName('startDateTime') ;
-let fromZoneName =  matches[i].startDateTime;
-
-        let toZoneName = 'UTC -04:00'; //hardcoded for now
-        
-         const timeZoneConvert = async ()=>{
-        let query = `https://api.ipgeolocation.io/timezone?apiKey=0ac1774f52d842aeb0cf8aa70fc7925e&location=Toronto`;
- 
-
-    
-fetch(query).then(response =>response.json().then(res=>{
-    let offset = res.timezone_offset;
-    
-}))
-
-        //     // http://api.timezonedb.com/v2.1/convert-time-zone?key=YOUR_API_KEY&format=json&from=America/Los_Angeles&to=Australia/Sydney&time=1464793200
-
-
-        //     const base2 = 'https://api.timezonedb.com/v2.1/convert-time-zone'
-        
-        //     const query = `?key=${key2}&from=America/Los_Angeles&to=Australia/Sydney&time=1464793200`;
-        
-        //     const response = await fetch(base2 + query);
-            
-        //     console.log(response);
-        
-        };
-        timeZoneConvert();
-        // const timeZoneConvert = aysnc() => {
-        
-        //     const base2 = 'http://api.timezonedb.com/v2.1/convert-time-zone'
-        
-        //     const query = `&apiKey=${key2}&q=${fromZoneName}`;
-        
-        //     const response = await fetch(toZoneName);
-            
-        //     console.log(response);
-        // };
-
+    const settings =  {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "dev132-cricket-live-scores-v1.p.rapidapi.com",
+            "x-rapidapi-key": "6ecb70d40bmsh6e5782f07713658p17f146jsn581f137859e6"
+        }
     }
+    const data= await fetch(base, settings).then( r=>r.json() )
+
+
+    let city = document.querySelector("#loci").value; 
+
+    const offsetApi = await fetch( 
+        'https://api.ipgeolocation.io/timezone?apiKey=0ac1774f52d842aeb0cf8aa70fc7925e&location='+city).then( r=>r.json() )
+
+        
+    
+    const offsetMinutes = offsetApi.timezone_offset*60
+
+    //make sure tableBody is empty
+    $("#tableBody").empty();
+
+    // let matches = data.matchList.matches.map(function(match){
+    //     return {series: match.series, date: match.startDateTime}
+    // });
+
+    console.log(data.matchList);
+
+
+    data.matchList.matches.forEach(function(match){
+        const timeUTC = match.startDateTime
+        const timeToronto = moment.utc(timeUTC).utcOffset(offsetMinutes).format('YYYY/MM/DD HH:mm:ss')
+        //create the html to add to the DOM
+        const html = `<tr>
+            <td>${match.series.name}</td>
+            <td>${timeUTC}</td>
+            <td>${timeToronto}</td>
+        </tr>`
+
+        //add html to DOM
+        $("#tableBody").append(html);
+    })
 };
+
+
 
 getSport('cricket')
 .then(data => console.log(data));
 
 
-// //API call for timezone conversion
-
-// const key2 = '635GDFGAGG0L';
-
-// let fromZoneName = data.matchList.matches.series.getElementByTagName('startDateTime') ;
-
-// let toZoneName = 'UTC-4'; //hardcoded for now
-
-// const timeZoneConvert = async ()=>{
-
-//     const base2 = 'http://api.timezonedb.com/v2.1/convert-time-zone'
-
-//     const query = `&apiKey=${key2}&q=${fromZoneName}`;
-
-//     const response = await fetch(toZoneName);
-    
-//     console.log(response);
-
-// };
-
-// const timeZoneConvert = aysnc() => {
-
-//     const base2 = 'http://api.timezonedb.com/v2.1/convert-time-zone'
-
-//     const query = `&apiKey=${key2}&q=${fromZoneName}`;
-
-//     const response = await fetch(toZoneName);
-    
-//     console.log(response);
-// };
